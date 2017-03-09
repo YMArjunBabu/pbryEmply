106b44b7c14798f152650dd06119cf94b4c9b47f
arjun.ym506@gmail.com <arjun.ym506@gmail.com>
2017-03-09 17:05:30 +0530

    Basic server file

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-# pbryEmply
+var express = require('express');
+var app = express();
+app.get('/',function(request,response){
+    response.send("Hello we are placing Pressbury Employee Enrollment Application here");
+});
+app.listen('5678',function(){
+    var port = 5678;
+    var host = "localhost"
+    console.log("Pressbury Employe Enrollment Application is running on  http://%s:%s",host,port);
+});
