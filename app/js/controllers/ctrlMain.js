0a0c0e6ab65df14810f2fdfa056555757a3c8dc0
arjun.ym506@gmail.com <arjun.ym506@gmail.com>
2017-03-13 12:50:59 +0530

    validations

--- app/js/controllers/ctrlMain.js
+++ app/js/controllers/ctrlMain.js
@@ -16,17 +16,22 @@ function mainCtrl($scope,$http){
     });
   };
   $scope.regs = function(){
-    $http({
-      method:"POST",
-      url:"http://192.168.10.108:5678/reg",
-      data : $scope.objSignUp
-    }).then(function successCallback(response) {
-      console.log(response,"post");
-      $scope.objSignUp = {};
-//       $scope.aryList = response.data;
-    }, function errorCallback(response) {
-      console.log(response,"error");
-    });
+    if($scope.objSignUp.firstname == "" || $scope.objSignUp.lastname == ""){
+       alert("Please fill the fields");
+    }else{
+         $http({
+        method:"POST",
+        url:"http://192.168.10.108:5678/reg",
+        data : $scope.objSignUp
+      }).then(function successCallback(response) {
+        console.log(response,"post");
+        $scope.objSignUp = {};
+  //       $scope.aryList = response.data;
+      }, function errorCallback(response) {
+        console.log(response,"error");
+      });
+    }
+
   };
 
 };
